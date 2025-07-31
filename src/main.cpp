eb331f3eadcbb99fa77b3bf404d69c69eee0b36b
radhapro <radha.robozz@gmail.com>
2025-07-31 15:41:19 +0530

    program 2 : Updated code to only receive MQTT messages

--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,42 @@
 // --- YE 2 LINE AAPKO BADALNI HAIN ---
 const char* ssid = "Robozz Lab";
 const char* password = "Robotics@cloud";
-// ------------------------------------
- // Dekho, yahan bhi humne wahi address likha hai jo MQTTBox me likha tha
+  // ------------------------------------
+
 const char* mqtt_server = "broker.hivemq.com";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+// YE FUNCTION TAB CHALEGA JAB KOI MESSAGE AAYEGA
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Ek naya message aaya hai! Topic: ");
+  Serial.println(topic);
+
+  Serial.print("Message: ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+  Serial.println("-----------------------");
+}
+
 // Ye function dobara connect hone ki koshish karega
 void reconnect() {
-  // Jab tak connection na jud jaye, koshish karte raho
   while (!client.connected()) {
     Serial.print("MQTT Broker se judne ki koshish...");
-    
-    // Iska naam alag rakha hai, taaki computer se clash na ho
-    if (client.connect("Mera_ESP32")) {
+    if (client.connect("Mera_ESP32_Full")) {
       Serial.println(" jud gaya!");
+      
+      // Connection judte hi, is channel ko sunna shuru kar do
+      client.subscribe("esp32/command");
+      Serial.println("Ab main 'esp32/command' topic ko sun raha hoon...");
+
     } else {
       Serial.print("failed, state=");
       Serial.print(client.state());
       Serial.println(" 5 sec baad fir koshish");
-      delay(5000); // 5 second ruk kar fir se try karega
+      delay(5000);
     }
   }
 }
@@ -33,36 +48,24 @@ void reconnect() {
 void setup() {
   Serial.begin(115200);
   delay(10);
-
-  // WiFi se connect karo
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("");
-  Serial.println("WiFi jud gaya!");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  // MQTT Broker ka address aur port set karo
+  Serial.println(" WiFi jud gaya!");
   client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 }
 
 void loop() {
-  // Agar connection toota hua hai, to reconnect function ko bulao
   if (!client.connected()) {
     reconnect();
   }
-  
-  // Ye line bohot zaroori hai. Ye connection ko zinda rakhti hai.
   client.loop();
-  
-    // --- YE 2 NAYI LINE ADD KI HAIN ---
-  delay(5000); // 5 second ruko
-  client.publish("esp32/status", "Hi, main connected hoon!");
-  // ------------------------------------
-} 
+
+  // Har 10 second me ek status message bhejo
+  delay(10000);
+  client.publish("esp32/status", "Main zinda hoon aur sun raha hoon!");
+  Serial.println("Status message bheja.");
+}
